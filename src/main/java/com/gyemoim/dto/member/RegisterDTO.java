d73c5bea0c46a8978b8d7d94ff7601fa6929942f
JaeHong94 <moonshin123@naver.com>
2023-03-06 16:36:48 +0900

    modify: 회원가입 코드 수정

--- src/main/java/com/gyemoim/dto/member/RegisterDTO.java
+++ src/main/java/com/gyemoim/dto/member/RegisterDTO.java
@@ -1,61 +1,78 @@
 package com.gyemoim.dto.member;
 
 public class RegisterDTO {
-    String email; //이메일
-     String password; //패스워드
-     String name; //이름
-    String ssn; //주민등록번호
-    String phone; //전화번호
-    String address; //주소
+  String email; //이메일
+  String password; //패스워드
+  String name; //이름
+  String ssn; //주민등록번호
+  String phone; //전화번호
+  String address; //주소
+  String addresscode;// 우편번호
+  String addressdetail;// 상세주소
 
+  public String getAddresscode() {
+    return addresscode;
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  public void setAddresscode(String addresscode) {
+    this.addresscode = addresscode;
+  }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+  public String getAddressdetail() {
+    return addressdetail;
+  }
 
-    public String getPassword() {
-        return password;
-    }
+  public void setAddressdetail(String addressdetail) {
+    this.addressdetail = addressdetail;
+  }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+  public String getEmail() {
+    return email;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public void setEmail(String email) {
+    this.email = email;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public String getPassword() {
+    return password;
+  }
 
-    public String getSsn() {
-        return ssn;
-    }
+  public void setPassword(String password) {
+    this.password = password;
+  }
 
-    public void setSsn(String ssn) {
-        this.ssn = ssn;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public String getPhone() {
-        return phone;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
+  public String getSsn() {
+    return ssn;
+  }
 
-    public String getAddress() {
-        return address;
-    }
+  public void setSsn(String ssn) {
+    this.ssn = ssn;
+  }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
 
 
 }
